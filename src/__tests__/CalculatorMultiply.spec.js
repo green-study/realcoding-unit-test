93d26df6f6e8d96a16e52155d615b1fb1e641d71
BHwi <bhwi0422@gmail.com>
2022-05-02 17:59:34 +0900

    add : div / 0

--- src/__tests__/CalculatorMultiply.spec.js
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -1,7 +1,8 @@
 import Calculator from "../js/calculator/Calculator";
 
+const cal = new Calculator();
 describe("사칙연산 곱셈 테스트.", () => {
   test("3과 6을 곱하면 18이다.", () => {
-    expect(3 * 6).toEqual(18);
+    expect(cal.multiply(a, b)).toEqual(18);
   });
 });

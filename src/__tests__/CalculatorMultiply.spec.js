5a1ffe9275cc453ffd2d125f30147dbb8bc1a62f
green-study <soo010410@naver.com>
2022-05-02 23:24:17 +0900

    hw

--- src/__tests__/CalculatorMultiply.spec.js
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -2,8 +2,8 @@ import Calculator from "../js/calculator/Calculator";
 
 const cal = new Calculator();
 describe("사칙연산 곱셈 테스트.", () => {
-  let a = 4;
-  let b = 2;
+  let a = 3;
+  let b = 6;
 
   test("3과 6을 곱하면 18이다.", () => {
     expect(cal.multiply(a, b)).toEqual(18);

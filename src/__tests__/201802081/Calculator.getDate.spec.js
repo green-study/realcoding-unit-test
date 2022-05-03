09da1e44447a0fd24892696dab3415750236d3fc
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-05-03 21:39:14 +0900

    add : additional assignment

--- src/__tests__/201802081/Calculator.getDate.spec.js
+++
@@ -1,8 +0,0 @@
-import Calculator from "../../js/calculator/Calculator";
-
-describe("Date 클래스 판단하기", () => {
-  test("getDate를 하면 Date객체가 나온다.", () => {
-    const cal = new Calculator();
-    expect(cal.getDate()).toBeInstanceOf(Date);
-  });
-});

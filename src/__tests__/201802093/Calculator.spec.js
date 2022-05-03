cd15b75c95800b49c0d302726aecf09f251bde44
BHwi <43560497+BHwi@users.noreply.github.com>
2022-05-03 19:48:11 +0900

    Update Calculator.spec.js

--- src/__tests__/201802093/Calculator.spec.js
+++ src/__tests__/201802093/Calculator.spec.js
@@ -2,10 +2,20 @@ import Calculator from "../../js/calculator/Calculator";
 
 const cal = new Calculator();
 
-console.log("Calculator Test");
+beforeAll(() => {
+  console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
+});
+afterAll(() => {
+  console.log("모든 테스트가 완료된 후 한번만 실행된다.");
+});
+beforeEach(() => {
+  console.log("각 테스트를 실행하기 전 실행한다.");
+});
+afterEach(() => {
+  console.log("각 테스트가 완료된 후 실행된다.");
+});
 
 describe("음수 양수 판단", () => {
-  console.log("음수 양수 판단 start");
   test("isNegative에 -1을 넣으면 true를 반환한다.", () => {
     expect(cal.isNegative(-1)).toEqual(true);
   });
@@ -15,16 +25,11 @@ describe("음수 양수 판단", () => {
   test("isNegative에 1을 넣으면 false를 반환한다.", () => {
     expect(cal.isNegative(1)).toEqual(false);
   });
-  console.log("음수 양수 판단 end");
 });
 
 describe("Date 클래스 판단하기", () => {
-  console.log("Date 클래스 판단 start");
   test("getDate를 하면 Date 객체가 나온다.", () => {
     let date = cal.getDate();
     expect(cal.getDate() instanceof Date).toEqual(true);
   });
-  console.log("Date 클래스 판단 end");
 });
-
-console.log("Calculator Test 종료");

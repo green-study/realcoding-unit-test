543d1c303e29d7c5b3c35a25e43349358bbb82b9
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-05-03 19:45:22 +0900

    add : console.log

--- src/__tests__/201802081/Calculator.isNegative.spec.js
+++ src/__tests__/201802081/Calculator.isNegative.spec.js
@@ -1,5 +1,21 @@
 import Calculator from "../../js/calculator/Calculator";
 
+beforeAll(() => {
+  console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
+});
+
+afterAll(() => {
+  console.log("모든 테스트가 완료된 후 한번만 실행된다.");
+});
+
+beforeEach(() => {
+  console.log("각 테스트를 실행하기 전 실행한다.");
+});
+
+afterEach(() => {
+  console.log("각 테스트가 완료된 후 실행된다.");
+});
+
 describe("음수 양수 판단", () => {
   test("isNegative에 7을 넣으면 false를 반환한다.", () => {
     const cal = new Calculator();

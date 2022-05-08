46bdcf568de1b6ad0f35bf61121da1c61782438b
green-study <soo010410@naver.com>
2022-05-08 20:53:58 +0900

    함수호출 업데이트

--- src/__tests__/202002471/Calculator.spec.js
+++ src/__tests__/202002471/Calculator.spec.js
@@ -20,13 +20,13 @@ const cal = new Calculator();
 describe("음수 양수 판단", () => {
   
     test("isNegative에 7을 넣으면 false를 반환한다.", () => {
-      expect(cal.isNegative(7)).toBeFalsy;
+      expect(cal.isNegative(7)).toBeFalsy();
     });
     test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
-        expect(cal.isNegative(-3)).toBeTruthy;
+        expect(cal.isNegative(-3)).toBeTruthy();
     });
     test("isNegative에 0을 넣으면 true가 아니다.", () => {
-        expect(cal.isNegative(0)).not.toBeTruthy;
+        expect(cal.isNegative(0)).not.toBeTruthy();
     });
 });
 

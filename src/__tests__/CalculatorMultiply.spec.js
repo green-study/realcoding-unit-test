8f6043d8b362a7d77287912960ff8f5a40124390
LONGNEW <40235475+LONGNEW@users.noreply.github.com>
2022-05-08 20:15:22 +0900

    Update CalculatorMultiply.spec.js

--- src/__tests__/CalculatorMultiply.spec.js
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -6,6 +6,6 @@ describe("사칙연산 곱셈 테스트.", () => {
   let b = 6;
 
   test("3과 6을 곱하면 18이다.", () => {
-    expect(cal.multiply(a, b)).toEqual(a * b);
+    expect(cal.multiply(a, b)).toEqual(18);
   });
 });
